43c3c8e60f317212e2907f23443d14b986b0736d
kherlen <hrln06@gmail.com>
2025-01-22 16:55:57 +0800

    Docs folder gitpage ashiglahiin tuld uusgelee

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,9 @@
-import axios from "axios";
+require("@babel/polyfill");
+import Search from "./model/search";
 
-async function doSearch(search) {
-  try {
-    let result = await axios("https://forkify-api.herokuapp.com/api/v2/recipes?search=" + search + "&c77efedb-e3fd-40bc-9d2a-fe4de39490d8=%3Cc77efedb-e3fd-40bc-9d2a-fe4de39490d8%3E#");
-  const recipes = result.data.data.recipes;
-  console.log(recipes);
+let search = new Search("pasta");
+search.doSearch().then(r => console.log(r));
 
-  result = await axios("https://forkify-api.herokuapp.com/api/v2/recipes/" + recipes[1] + "?c77efedb-e3fd-40bc-9d2a-fe4de39490d8=<c77efedb-e3fd-40bc-9d2a-fe4de39490d8>");
-  console.log(result);
-  } catch(error) { alert("Asuudal garlaa: " + error); }
-}
-doSearch('pizza');
 
 
 
--- src/js/model/search.js
+++ src/js/model/search.js
@@ -1,3 +1,18 @@
-let query = 'pizza';
+require("@babel/polyfill");
+import axios from "axios";
+export default class Search {
+constructor(query) {
+    this.query = query;
+}
+async doSearch(query) {
+    try {
+        let result = await axios(
+        "https://forkify-api.herokuapp.com/api/search?q=" + this.query);
+        this.result = result.data.recipes;
+        return this.result;
 
-export default query;
+    } catch (error) {
+        alert("Aldaa garlaa : " + error);
+    }
+    }
+}
